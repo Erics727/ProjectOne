4f29275ba929a74ee6ea04d8a3efd838017e96bf
Brett Garber <bgarber0343@gmail.com>
2021-10-02 10:13:55 -0500

    Started implementing Controllers and Services

---
+++ p1-eshawn727/src/main/java/com/revature/controllers/ReimbursementController.java
@@ -0,0 +1,44 @@
+package com.revature.controllers;
+
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.revature.models.Reimbursement;
+import com.revature.models.ReimbursementStatus;
+import com.revature.services.ReimbursementService;
+
+import io.javalin.http.Handler;
+
+public class ReimbursementController {
+	
+	ReimbursementService rs = new ReimbursementService();
+	
+	public Handler getAllReimsHandler = (ctx) -> {
+		
+		//if a session exists
+		if(ctx.req.getSession(false) != null) {
+			
+			//create an array with reim data
+			List<Reimbursement> allReims = rs.getAllReims();
+			
+			//instantiate a Gson object to convert between JSON and POJO
+			Gson gson = new Gson();
+			
+			//convert the java object to a JSON string
+			String JSONReims = gson.toJson(allReims);
+			
+			//return reimbursement data
+			ctx.result(JSONReims);
+			
+			//send a 200 code(success)
+			ctx.status(200);
+			
+		} else {
+			
+			//else send a forbidden code
+			ctx.status(403);
+		}
+		
+	};
+
+}
---
+++ p1-eshawn727/src/main/java/com/revature/services/ReimbursementService.java
@@ -0,0 +1,43 @@
+package com.revature.services;
+
+import java.util.List;
+
+import com.revature.daos.ReimbursementDao;
+import com.revature.models.Reimbursement;
+import com.revature.models.ReimbursementStatus;
+import com.revature.models.Users;
+
+public class ReimbursementService {
+	
+	//instantiate a ReimbursementDao
+	ReimbursementDao rDao = new ReimbursementDao();
+	
+	//method will return all reimbursement data from the Dao to the Controller
+	public List<Reimbursement> getAllReims() {
+		
+		return rDao.getAllReims();
+		
+	}
+	
+	//method will return all reimbursement data by status from the Dao to the Controller
+	public List<Reimbursement> getReimsByStatus(ReimbursementStatus status) {
+		
+		return rDao.getReimsByStatus(status);
+		
+	}
+	
+	//method will return all reimbursement data by user id from the Dao to the Controller
+	public List<Reimbursement> getReimsById(Users user) {
+		
+		return rDao.getReimsById(user);
+		
+	}
+	
+	//method will return all reimbursement data by status id from the Dao to the Controller
+	public List<Reimbursement> getReimsByStatusId(int id) {
+		
+		return rDao.getReimsByStatusId(id);
+		
+	}
+
+}
